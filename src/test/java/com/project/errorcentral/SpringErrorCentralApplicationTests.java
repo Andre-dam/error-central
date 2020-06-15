fd63ad9b9b63490235d5361f9505b1fd7efea2f8
andre.junior <aldsj@cin.ufpe.br>
2020-06-14 23:28:49 -0300

    first commit

---
+++ src/test/java/com/project/errorcentral/SpringErrorCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.errorcentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringErrorCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
